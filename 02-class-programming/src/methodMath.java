f507dd916ddfcaa2eb694a0aa37a8c7fadf594a0
kate keiroz <katekeiroz@gmail.com>
2025-03-04 10:47:12 +0000

    method class

---
+++ 02-class-programming/src/methodMath.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+
+public class methodMath {
+    public static void main(String[] args){
+        double pi = Math.PI;
+        int absValue = Math.abs(-10);
+        double sqrtValue = Math.sqrt(25);
+        double powerValue = Math.pow(2,3);
+
+        System.out.println("PI: " + pi);
+        System.out.println("Absolute Value: " + absValue);
+        System.out.println("Square root: " + sqrtValue);
+        System.out.println("Power Value: " + powerValue);
+    }
+}
